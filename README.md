034b036e723692e05baa75eade98e58e2debf160
abobakaraslam <m.abobakar.aslam@gmail.com>
2024-03-24 10:55:37 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lesson5example5
+ Example 5 Lesson 5 Basic Codes for Tailwind CSS in React
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+/* File: App.jsx */
+function App() {
+  return (
+    <h1 className="bg-slate-950 text-white sm:bg-yellow-800 md:bg-red-900 lg:bg-lime-800 xl:bg-purple-900 2xl:bg-cyan-700">
+      Different colors for different devices
+    </h1>
+  );
+}
+export default App;
